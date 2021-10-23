9ab597fe57b415ff62cc1c15addbc061202cde2c
glebassty <glebassty@gmail.com>
2021-10-23 23:42:46 +0300

    add Sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { Box, VStack, StackDivider } from "@chakra-ui/react";
+import React from "react";
+import Link from "next/link";
+
+const data = [
+  { id: 1, title: "Матчи", link: "matches" },
+  { id: 1, title: "Игроки", link: "players" },
+];
+
+const Sidebar = () => {
+  return (
+    <VStack
+      divider={<StackDivider borderColor='gray.200' />}
+      spacing={4}
+      align='stretch'
+      width='100%'
+    >
+      {data.map((item) => (
+        <Link key={item.id} href={`/content/${item.link}`}>
+          <a>
+            <Box>{item.title}</Box>
+          </a>
+        </Link>
+      ))}
+    </VStack>
+  );
+};
+
+export default Sidebar;
