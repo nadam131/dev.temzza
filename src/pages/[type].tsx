617d354236f844212f5aecd93155e552b5542432
glebassty <glebassty@gmail.com>
2021-10-26 21:46:06 +0300

    add pages - [type]

---
+++ src/pages/[type].tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Matches from "../composite/Matches/Matches";
+import Players from "../composite/Players/Players";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const PageType = () => {
+  const {
+    query: { type },
+  } = useRouter();
+
+  const lastMatches = useSWR(
+    "https://eplg-nest-nadam131.vercel.app/fixtures/last",
+    fetcher
+  );
+
+  const nextMatches = useSWR(
+    "https://eplg-nest-nadam131.vercel.app/fixtures/next",
+    fetcher
+  );
+
+  return (
+    <div>
+      {type === "matches" ? (
+        <Matches
+          lastMatches={lastMatches}
+          nextMatches={nextMatches}
+          type={type}
+        />
+      ) : (
+        <Players type={type} />
+      )}
+    </div>
+  );
+};
+
+export default PageType;
