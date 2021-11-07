c547bb8023dffec2905714d8d34e3bf1b63e6bc4
nadam131 <nickadam131@gmail.com>
2021-11-07 23:02:11 +0300

    remove unused colors theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,13 +3,11 @@ import { extendTheme } from "@chakra-ui/react";
 import styles from "./styles";
 import sizes from "./foundations/sizes";
 import fonts from "./foundations/fonts";
-import colors from "./foundations/colors";
 
 const overrides = {
   ...styles,
   ...sizes,
   ...fonts,
-  ...colors,
 };
 
 export default extendTheme(overrides);
